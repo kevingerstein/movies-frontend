a5737ef4f22d1a5a61b646c948bd0386e05707dd
kevin <kg1941a@gmail.com>
2022-02-09 23:35:23 -0500

    fixes CRUD actions and adds html dialog box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,10 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Movies!",
       movies: [],
       newMovie: {},
-      movieInfo: {},
-      id: null,
+      currentMovie: {},
+      editMovieParams: {},
     };
   },
   created: function () {
@@ -15,37 +14,39 @@ export default {
   },
   methods: {
     indexMovies: function () {
-      this.movies.push = axios.get("http://localhost:3000/movies").then((response) => (this.movies = response.data));
+      axios.get("http://localhost:3000/movies").then((response) => (this.movies = response.data));
     },
     createMovie: function () {
-      axios.post("http://localhost:3000/movies", this.newMovie).then((response) => {
-        this.movies.push(response.data);
-        this.newMovie = {};
-      });
-    },
-    showMovie: function (id) {
-      axios.get(`http://localhost:3000/movies/${id}`).then((response) => {
-        this.movieInfo = response.data;
-        this.id = this.movieInfo.id;
-      });
-    },
-    updateMovie: function (id) {
       axios
-        .patch(`http://localhost:3000/movies/${id}`, this.movieInfo)
+        .post("http://localhost:3000/movies", this.newMovie)
         .then((response) => {
-          this.movies.forEach((movie, index) => {
-            if (movie.id == response.data.id) {
-              movie = response.data;
-              this.movies[index] = response.data;
-            }
-          });
+          this.movies.push(response.data);
+          this.newMovie = {};
         })
-        .catch((error) => console.log(error.response));
+        .catch((error) => console.log(error.response.data.errors));
     },
-    destroyMovie: function (id) {
-      axios.delete(`http://localhost:3000/movies/${id}`).then(() => {
-        this.movies = this.movies.filter((movie) => movie.id !== id);
-      });
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      this.editMovieParams = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+    updateMovie: function (movie) {
+      axios
+        .patch(`http://localhost:3000/movies/${movie.id}`, movie)
+        .then((response) => console.log(response.data))
+        .catch((error) => console.log(error.response.data.errors));
+    },
+    destroyMovie: function (movie) {
+      if (confirm("Are you sure you to delete this?")) {
+        axios
+          .delete(`http://localhost:3000/movies/${movie.id}`)
+          .then(() => {
+            let index = this.movies.indexOf(movie);
+            this.movies.splice(index, 1);
+          })
+          .catch((error) => console.log(error.response.data.errors));
+      }
     },
   },
 };
@@ -53,56 +54,60 @@ export default {
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
     <div v-for="movie in movies" :key="movie.id">
       <h3>{{ movie.title }}</h3>
-      <button v-on:click="showMovie(movie.id)">See info</button>
-      <button v-on:click="destroyMovie(movie.id)">Delete Movie</button>
-      <div v-if="movie.id === movieInfo.id">
-        Movie year: {{ movieInfo.year }}
-        <br />
-        Movie director: {{ movieInfo.director }}
-        <br />
-        Movie plot: {{ movieInfo.plot }} Movie
-        <br />
-        <div>
-          <h3>Update below...</h3>
-          <label for="title">Title:</label>
-          <input type="text" v-model="movieInfo.title" id="title" />
-          <br />
-          <label for="title">Year:</label>
-          <input type="number" v-model="movieInfo.year" min="1800" max="2022" id="year" />
-          <br />
-          <label for="title">Plot:</label>
-          <input type="text" v-model="movieInfo.plot" id="plot" />
-          <br />
-          <label for="title">Director:</label>
-          <input type="text" v-model="movieInfo.director" id="director" />
-          <br />
-          <label for="title">English?</label>
-          <input type="checkbox" v-model="movieInfo.english" id="english" />
-          <br />
-          <button v-on:click="updateMovie(id)">Update Movie</button>
-        </div>
-      </div>
+      <button v-on:click="showMovie(movie)">See info</button>
     </div>
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h2>Movie Info</h2>
+        <p>
+          Title:
+          <input type="text" v-model="editMovieParams.title" />
+        </p>
+        <p>
+          Plot:
+          <input type="text" v-model="editMovieParams.plot" />
+        </p>
+        <p>
+          Director:
+          <input type="text" v-model="editMovieParams.director" />
+        </p>
+        <p>
+          Year:
+          <input type="text" v-model="editMovieParams.year" />
+        </p>
+        <p>
+          In English:
+          <input type="checkbox" v-model="editMovieParams.english" id="english" />
+        </p>
+        <button v-on:click="updateMovie(editMovieParams)">Update</button>
+        <button v-on:click="destroyMovie(editMovieParams)">Delete</button>
+        <button>Close</button>
+      </form>
+    </dialog>
     <div>
       <h2>Add a new Movie below...</h2>
-      <label for="title">Title:</label>
-      <input type="text" v-model="newMovie.title" id="title" />
-      <br />
-      <label for="title">Year:</label>
-      <input type="number" v-model="newMovie.year" min="1800" max="2022" id="year" />
-      <br />
-      <label for="title">Plot:</label>
-      <input type="text" v-model="newMovie.plot" id="plot" />
-      <br />
-      <label for="title">Director:</label>
-      <input type="text" v-model="newMovie.director" id="director" />
-      <br />
-      <label for="title">English?</label>
-      <input type="checkbox" v-model="newMovie.english" id="english" />
-      <br />
+      <p>
+        Title:
+        <input type="text" v-model="newMovie.title" id="title" />
+      </p>
+      <p>
+        Year:
+        <input type="number" v-model="newMovie.year" min="1800" max="2022" id="year" />
+      </p>
+      <p>
+        Plot:
+        <input type="text" v-model="newMovie.plot" id="plot" />
+      </p>
+      <p>
+        Director:
+        <input type="text" v-model="newMovie.director" id="director" />
+      </p>
+      <p>
+        In English:
+        <input type="checkbox" v-model="newMovie.english" id="english" />
+      </p>
       <button v-on:click="createMovie()">Add New Movie</button>
     </div>
   </div>
