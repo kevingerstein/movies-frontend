2c93d087d4d9d986e77af27535cf7f43023d3c6f
kevin <kg1941a@gmail.com>
2022-02-25 00:01:09 -0500

    adds bootstrap, nav bar, and card groups. Attempted carousels with actor images and got it working BUT the controls on carousels still need to be fixed

--- src/App.vue
+++ src/App.vue
@@ -17,39 +17,44 @@ export default {
 </script>
 
 <template>
-  <div id="nav">
-    <router-link v-if="!isLoggedIn" to="/signup">Signup</router-link>
-    |
-    <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
-    |
-    <router-link v-if="isLoggedIn" to="/logout">Logout</router-link>
-    |
-    <router-link to="/movies">Movies</router-link>
-    |
-    <router-link to="/movies/new">New Movie</router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" v-if="!isLoggedIn" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" v-if="!isLoggedIn" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" v-if="isLoggedIn" to="/logout">Logout</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/movies">Movies</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/movies/new">New Movie</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -29,6 +29,74 @@ export default {
 </script>
 
 <template>
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col" v-for="movie in movies" :key="movie.id">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{ movie.title }}</h5>
+          <p class="card-text">
+            {{ movie.plot }}
+          </p>
+          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div class="carousel-inner">
+              <div
+                class="carousel-item"
+                :class="{ active: index === 0 }"
+                v-for="(actor, index) in movie.actors"
+                :key="actor.id"
+              >
+                <img :src="actor.image" class="d-block w-100" alt="..." />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>{{ actor.first_name + " " + actor.last_name }}</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="prev"
+            >
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="next"
+            >
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="index-movies">
     <label for="titleFilter">Search:</label>
     <input type="text" v-model="titleFilter" list="titles" id="titleFilter" />
